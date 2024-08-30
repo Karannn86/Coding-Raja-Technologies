4415015883c56daf0c9116afe913724d4661a8aa
Karannn86 <karannew86@gmail.com>
2024-08-30 23:32:04 +0530

    Add files via upload

---
+++ Resume-Builder/src/home/index.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/ui/custom/Header'
+import React from 'react'
+
+function Home() {
+  return (
+    <div style={{height:"100vh"}}> 
+      <Header/>
+      <img style={{width:"100%",height:"100%"}} src='AI-Resume-Builder.jpg'/>
+    </div>
+  )
+}
+
+export default Home
